d4979753396c96a4ae800ec5138676fd170826b7
jtrtj <jt.r@live.com>
2018-08-14 14:32:11 -0600

    start writing merchants analytics tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe Merchant, type: :model do
-  it {should validate_presence_of(:name)}
-  it {should validate_presence_of(:created_at)}
-  it {should validate_presence_of(:updated_at)}
+  context 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:created_at)}
+    it {should validate_presence_of(:updated_at)}
+  end
+  context 'relationships' do
+    it { should have_many(:invoices) } 
+  end
 end
---
+++ spec/requests/api/v1/merchants/merchants_analysis_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  context 'GET /api/v1/merchants/:id/revenue' do
+    it 'will display total revenue for a single merchant' do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      item = Item.create!(name: "VaporLord", description: "Wanna smork?", merchant: merchant, unit_price: 5000, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC" )
+      invoice_1 = merchant.invoices.create!(merchant: merchant, customer: customer, status: "shipped", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      invoice_2 = merchant.invoices.create!(merchant: merchant, customer: customer, status: "shipped", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      invoice_3 = merchant.invoices.create!(merchant: merchant, customer: customer, status: "shipped", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_1, item: item, unit_price: item.unit_price, quantity: 2, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_1, item: item, unit_price: item.unit_price, quantity: 1, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_2, item: item, unit_price: item.unit_price, quantity: 1, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_2, item: item, unit_price: item.unit_price, quantity: 1, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_3, item: item, unit_price: item.unit_price, quantity: 2, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      Transaction.create!(invoice: invoice_1, credit_card_number: 9876, credit_card_expiration_date: " ", result: "success", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      Transaction.create!(invoice: invoice_2, credit_card_number: 9876, credit_card_expiration_date: " ", result: "success", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      Transaction.create!(invoice: invoice_3, credit_card_number: 9876, credit_card_expiration_date: " ", result: "failed", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      require 'pry'; binding.pry
+    end
+  end
+end
