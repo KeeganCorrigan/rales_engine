9f684f2fbb78d0491f6acdddb6e262e4a9896c9b
jtrtj <jt.r@live.com>
2018-08-13 18:21:29 -0600

    generate invoice model

---
+++ spec/factories/invoices.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :invoice do
+    customer nil
+    merchant nil
+    status "MyString"
+    created_at "2018-08-13 18:19:50"
+    updated_at "2018-08-13 18:19:50"
+  end
+end
