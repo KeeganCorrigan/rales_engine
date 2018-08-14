997575f43c5ddb7bcfb2c201ac67cc4b8cfe330a
jtrtj <jt.r@live.com>
2018-08-14 15:23:11 -0600

    start writing revenue query for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,13 @@ class Merchant < ApplicationRecord
   validates_presence_of :name, :updated_at, :created_at
 
   has_many :invoices
+
+  def revenue
+    require 'pry'; binding.pry
+    invoices
+    .joins(:invoice_items, :transactions)
+  end
 end
+
+# .where("transactions.status = 'success'")
+#.select("SUM(invoice_items.quantity * invoice_items.unit_price")
