56943fb2161cad0c38852d7fb5611a5f2c67df15
jtrtj <jt.r@live.com>
2018-08-13 18:45:38 -0600

    write and pass model test for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:created_at) }
+    it { should validate_presence_of(:updated_at) }
+  end
+
+  context 'relationships' do
+  
+  end
 end
