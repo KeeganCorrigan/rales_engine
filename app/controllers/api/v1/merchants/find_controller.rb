4c5fa6c3722b8aa393ebf39a91f2d5777d432c73
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-14 13:30:41 -0600

    adds tests for find all merchants, adds routes, adds controller method for index

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::Merchants::FindController < ApplicationController
     render json: Merchant.find_by(find_params)
   end
 
+  def index
+    render json: Merchant.where(find_params)
+  end
+
   def find_params
     params.permit(:id, :name, :created_at, :updated_at)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
       namespace :merchants do
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
       end
-      
+
       resources :merchants, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :transactions, only: [:index, :show]
