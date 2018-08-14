eadd025ba99e3d9a4b44f381c41cdc72a660bf95
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-14 13:43:07 -0600

    adds tests for random merchant controller, adds route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/random.json', to: 'random#show'
       end
 
       resources :merchants, only: [:index, :show]
