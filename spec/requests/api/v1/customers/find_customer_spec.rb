1ea36f6c6502927b3c5f3247931db1dd5e82e8bc
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-14 19:43:38 -0600

    tests find by for all params for customer

--- spec/requests/api/v1/customers/find_customer_spec.rb
+++ spec/requests/api/v1/customers/find_customer_spec.rb
@@ -34,6 +34,52 @@ describe "Customers API" do
       expect(customer[:first_name]).to eq(first_name)
     end
 
+    it "returns a customer based on last name param" do
+      customers = create_list(:customer, 3)
 
+      customer = customers.first
+      first_name = customer.first_name
+      id = customer.id
+
+      get "/api/v1/customers/find?last_name=#{customer.last_name}"
+
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(customer[:id]).to eq(id)
+      expect(customer[:first_name]).to eq(first_name)
+    end
+
+    it "returns a customer based on updated at param" do
+      customers = create_list(:customer, 3)
+
+      customer = customers.first
+      first_name = customer.first_name
+      id = customer.id
+
+      get "/api/v1/customers/find?updated_at=#{customer.updated_at}"
+
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(customer[:id]).to eq(id)
+      expect(customer[:first_name]).to eq(first_name)
+    end
+
+    it "returns a customer based on created at param" do
+      customers = create_list(:customer, 3)
+
+      customer = customers.first
+      first_name = customer.first_name
+      id = customer.id
+
+      get "/api/v1/customers/find?created_at=#{customer.created_at}"
+
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(customer[:id]).to eq(id)
+      expect(customer[:first_name]).to eq(first_name)
+    end
   end
 end
