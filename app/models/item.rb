458873768b1ed2505d06f376545425b0fb7165ae
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-15 16:42:12 -0600

    add test for most_sold_by_day, add method for most_by_day, all tests pass but 1 unrelated

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   validates_presence_of :name, :description, :merchant_id, :unit_price, :created_at, :updated_at
+
   belongs_to :merchant
+
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+
+  def most_sold_by_day
+    # TODO: adjust presentation of data at serializer level to match spec harness expectations
+    invoices
+    .select("sum(invoice_items.quantity) AS total_quantity, invoices.*")
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.success)
+    .group("date_trunc('day', invoice_items.created_at), invoices.id")
+    .order("total_quantity DESC")
+    .limit(1)
+    .first
+    .created_at
+  end
 end
+
+# item.invoices.select("sum(invoice_items.quantity) AS total_quantity, invoices.*").joins(:invoice_items, :transactions).merge(Transaction.success).group
+# ("date_trunc('day', invoice_items.created_at), invoices.id").order("total_quantity DESC").limit(1).first.created_at
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,28 @@ RSpec.describe Item, type: :model do
   end
 
   context 'relationships' do
-    it { should belong_to(:merchant) } 
+    it { should belong_to(:merchant) }
+    it { should have_many(:invoices).through(:invoice_items)}
+  end
+
+  describe 'instance methods' do
+    it '.most_sold_by_day' do
+      merchant_1 = create(:merchant)
+
+      customer = create(:customer)
+
+      item = Item.create!(name: "VaporLord", description: "Wanna smork?", merchant: merchant_1, unit_price: 5000, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC" )
+
+      invoice_1 = merchant_1.invoices.create!(customer: customer, status: "shipped", created_at: "2012-03-10 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      invoice_2 = merchant_1.invoices.create!(customer: customer, status: "shipped", created_at: "2012-10-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+
+      InvoiceItem.create!(invoice: invoice_1, item: item, unit_price: item.unit_price, quantity: 2, created_at: "2012-03-10 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      InvoiceItem.create!(invoice: invoice_2, item: item, unit_price: item.unit_price, quantity: 1, created_at: "2012-10-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+
+      Transaction.create!(invoice: invoice_1, credit_card_number: 9876, credit_card_expiration_date: " ", result: "success", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+      Transaction.create!(invoice: invoice_2, credit_card_number: 9876, credit_card_expiration_date: " ", result: "success", created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+
+      expect(item.most_sold_by_day).to eq(invoice_1.created_at)
+    end
   end
 end
