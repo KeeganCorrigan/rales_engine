57109c720b4c177bbb967faa26df5a6e241a56d3
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-15 14:38:11 -0600

    fixes error in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815184534) do
+ActiveRecord::Schema.define(version: 20180814030901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20180815184534) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.date "created_at", null: false
-    t.date "updated_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invoice_items", force: :cascade do |t|
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20180815184534) do
     t.bigint "invoice_id"
     t.integer "quantity"
     t.integer "unit_price"
-    t.date "created_at"
-    t.date "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
     t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20180815184534) do
     t.bigint "customer_id"
     t.bigint "merchant_id"
     t.string "status"
-    t.date "created_at", null: false
-    t.date "updated_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
@@ -48,15 +48,15 @@ ActiveRecord::Schema.define(version: 20180815184534) do
     t.string "description"
     t.bigint "merchant_id"
     t.integer "unit_price"
-    t.date "created_at"
-    t.date "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
-    t.date "created_at"
-    t.date "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 20180815184534) do
     t.string "credit_card_number"
     t.string "credit_card_expiration_date"
     t.string "result"
-    t.date "created_at"
-    t.date "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
