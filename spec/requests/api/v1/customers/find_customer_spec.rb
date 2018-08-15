edac00435cab60f2c530311eae51ebf3e235e55d
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-14 19:39:30 -0600

    add controller for find customers, add test for finding customer by id and first name

--- spec/requests/api/v1/customers/find_customer_spec.rb
+++ spec/requests/api/v1/customers/find_customer_spec.rb
@@ -17,5 +17,23 @@ describe "Customers API" do
       expect(customer[:id]).to eq(id)
       expect(customer[:first_name]).to eq(first_name)
     end
+
+    it "returns a customer based on first name param" do
+      customers = create_list(:customer, 3)
+
+      customer = customers.first
+      first_name = customer.first_name
+      id = customer.id
+
+      get "/api/v1/customers/find?first_name=#{customer.first_name}"
+
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(customer[:id]).to eq(id)
+      expect(customer[:first_name]).to eq(first_name)
+    end
+
+
   end
-end      
+end
