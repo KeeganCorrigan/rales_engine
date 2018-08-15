6f1931691b250604d56f88ae56de445299850df3
jtrtj <jt.r@live.com>
2018-08-14 20:53:34 -0600

    write test for find by id

---
+++ spec/requests/api/v1/invoices/find_invoice_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'invoices API' do
+  context 'GET /api/v1/invoices/find?parameters' do
+    it 'returns an invoice based on id param' do
+      invoices = create_list(:invoice, 3)
+
+      invoice = invoices.first
+      id = invoice.id
+      customer_id = invoice.customer_id
+      merchant_id = invoice.merchant_id
+      status = invoice.status
+
+      get "/api/v1/invoices/find?id=#{id}"
+
+      invoice = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(invoice[:id]).to eq(id)
+      expect(invoice[:customer_id]).to eq(customer_id)
+      expect(invoice[:merchant_id]).to eq(merchant_id)
+      expect(invoice[:status]).to eq(status)
+    end
+  end
+end
