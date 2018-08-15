e29670e1df65ec4564a05625c7c6057c0845892c
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-14 18:31:37 -0600

    adds tests for transaction based on invoice id

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :transaction do
     invoice { create(:invoice) }
     credit_card_number "4654405418249632"
+    credit_card_expiration_date ""
     result "success"
     updated_at { 1.years.ago }
     created_at { 2.years.ago }
