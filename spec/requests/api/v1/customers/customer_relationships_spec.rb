c229942ea9063bdac7e900ef83e338e9286db177
jtrtj <jt.r@live.com>
2018-08-16 10:43:29 -0600

    finish relationship endpoints for customers, merchants and transactions

--- spec/requests/api/v1/customers/customer_relationships_spec.rb
+++ spec/requests/api/v1/customers/customer_relationships_spec.rb
@@ -26,18 +26,18 @@ describe 'Customers API' do
       merchant = create(:merchant)
       invoice_1 = create(:invoice, merchant: merchant, customer: customer)
       invoice_2 = create(:invoice, merchant: merchant, customer: customer)
-      trainsaction_1 = create(:transaction, invoice: invoice_1)
-      trainsaction_2 = create(:transaction, invoice: invoice_2)
+      transaction_1 = create(:transaction, invoice: invoice_1)
+      transaction_2 = create(:transaction, invoice: invoice_2)
 
       get "/api/v1/customers/#{customer.id}/transactions"
 
       transactions = JSON.parse(response.body, symbolize_names: true)
       
       expect(response).to be_successful
-      expect(transaction[0][:id]).to eq(transaction_1.id)
-      expect(transaction[0][:invoice_id]).to eq(transaction_1.invoice_id)
-      expect(transaction[1][:id]).to eq(transaction_2.id)
-      expect(transaction[1][:invoice_id]).to eq(transaction_2.invoice_id)
+      expect(transactions[0][:id]).to eq(transaction_1.id)
+      expect(transactions[0][:invoice_id]).to eq(transaction_1.invoice_id)
+      expect(transactions[1][:id]).to eq(transaction_2.id)
+      expect(transactions[1][:invoice_id]).to eq(transaction_2.invoice_id)
     end
   end
 end
